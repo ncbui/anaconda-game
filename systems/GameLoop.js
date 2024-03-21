762a666643fb98de44073a0131b19b581302691b
Cam Bui <29789422+ncbui@users.noreply.github.com>
2024-03-21 15:46:27 -0700

    Move expo app files to root

--- systems/GameLoop.js
+++ systems/GameLoop.js
@@ -2,6 +2,8 @@
 
 export default function (entities, { events, dispatch }) {
     const head = entities.head;
+    const food = entities.food;
+    const tail = entities.tail;
 
     if (events.length) {
       events.forEach((e) => {
@@ -33,9 +35,17 @@ export default function (entities, { events, dispatch }) {
     head.nextMove -= 1;
     if (head.newMove === 0) {
       head.nextMove = head.updateFrequency;
-      head.position[0] += head.xspeed;
-      head.position[1] += head.yspeed;
-    }
+      if (
+        head.position[0] + head.xspeed < 0  ||
+        head.position[0] + head.xspeed >= Constants.GRID_SIZE  ||
+        head.position[0] + head.yspeed < 0  ||
+        head.position[0] + head.yspeed >= Constants.GRID_SIZE
+       ) {
+        dispatch("game-over");
+        } else 
+        head.position[0] += head.xspeed;
+        head.position[1] += head.yspeed;
+      }
     
     return entities;
   }
