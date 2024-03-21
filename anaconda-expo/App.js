5616d1ae5888a0d9f59b9160c5fb6ca46c1fb911
Cam Bui <29789422+ncbui@users.noreply.github.com>
2024-03-21 06:09:54 +0000

    Add canvas

--- anaconda-expo/App.js
+++ anaconda-expo/App.js
@@ -1,20 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { GameEngine } from "react-native-game-engine";
+import React, { useRef } from "react";
+import Constants from "./Constants";
 
 export default function App() {
+  const BoardSize = Constants.GRID_SIZE * Constants.CELL_SIZE;
+  const engine = useRef(null);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.canvas}>
+      <GameEngine
+              ref={engine}
+              style={{
+                width: BoardSize,
+                height: BoardSize,
+                flex: null,
+                backgroundColor: "#EFE6E8",
+                borderRadius: 20,
+                borderWidth: 5,
+                borderColor: "black",
+                borderStyle: "solid"
+              }}
+            />
     </View>
-  );
-}
+  </View>
+)};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#4C243B',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 50,
   },
+  canvas: {
+    flex: 1,
+    backgroundColor: "#ECA400",
+    alignItems: "center",
+    justifyContent: "start",
+    padding: 50,
+    borderRadius: 20,
+    borderWidth: 10,
+    borderColor: "black",
+    borderStyle: "solid",
+  }
+
 });
