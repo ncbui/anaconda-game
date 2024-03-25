2cddbf6171c0ca911875685b9bb30a903c15fd23
cb <29789422+ncbui@users.noreply.github.com>
2024-03-24 17:15:21 -0700

    Update color scheme

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { AppRegistry, StyleSheet, Text, View } from "react-native";
 import { GameEngine } from "react-native-game-engine";
 import React, { useRef } from "react";
 import Constants from "./Constants";
@@ -15,50 +15,53 @@ const BoardSize = Constants.GRID_SIZE * Constants.CELL_SIZE;
 
 export default function App() {
   const engine = useRef(null);
-  
+
   return (
     <View style={styles.container}>
-    <View style={styles.canvas}>
-      <Text style={styles.title}>Anaconda</Text>
-      <GameEngine
-              ref={engine}
-              style={styles.board}
-              entities={{
-                head: {
-                  position: [0, 0],
-                  size: Constants.CELL_SIZE,
-                  updateFrequency: 10,
-                  nextMove: 10,
-                  xspeed: 0,
-                  yspeed: 0,
-                  renderer: <Head />,
-                },
-                food: {
-                  position: [
-                    randomPositions(0, Constants.GRID_SIZE - 2),
-                    randomPositions(0, Constants.GRID_SIZE - 2),
-                    randomPositions(0, Constants.GRID_SIZE - 2),
-                  ],
-                  size: Constants.CELL_SIZE,
-                  renderer: <Food />,
-                },
-                tail: {
-                  size: Constants.CELL_SIZE,
-                  elements: [],
-                  renderer: <Tail />,
-                },
-              }} 
-            />
+      <View style={styles.canvas}>
+        <Text style={styles.title}>Anaconda</Text>
+        <GameEngine
+          ref={engine}
+          style={styles.board}
+          entities={{
+            head: {
+              position: [0, 0],
+              size: Constants.CELL_SIZE,
+              updateFrequency: 10,
+              nextMove: 10,
+              xspeed: 0,
+              yspeed: 0,
+              renderer: <Head />,
+            },
+            food: {
+              position: [
+                randomPositions(0, Constants.GRID_SIZE - 2),
+                randomPositions(0, Constants.GRID_SIZE - 2),
+                randomPositions(0, Constants.GRID_SIZE - 2),
+              ],
+              size: Constants.CELL_SIZE,
+              renderer: <Food />,
+            },
+            tail: {
+              size: Constants.CELL_SIZE,
+              elements: [],
+              renderer: <Tail />,
+            },
+          }}
+        />
+      </View>
     </View>
-  </View>
-)};
+  );
+}
+
+AppRegistry.registerComponent('Anaconda', () => App);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#4C243B',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#B84A62",
+    alignItems: "center",
+    justifyContent: "center",
     padding: 50,
   },
   canvas: {
@@ -69,7 +72,7 @@ const styles = StyleSheet.create({
     padding: 50,
     borderRadius: 20,
     borderWidth: 10,
-    borderColor: "black",
+    borderColor: "#4C243B",
     borderStyle: "solid",
   },
   board: {
@@ -79,12 +82,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#EFE6E8",
     borderRadius: 20,
     borderWidth: 5,
-    borderColor: "black",
-    borderStyle: "solid"
+    borderColor: "#4C243B",
+    borderStyle: "solid",
+    padding: 2,
   },
   title: {
-
+    color: "#4C243B",
+    fontSize: 20,
+    fontWeight: "900",
+    padding: 10,
   },
-  
-
 });
