a665f3169bd6df7d578583ccfae28d88b0530b47
cb <29789422+ncbui@users.noreply.github.com>
2024-03-26 14:28:47 -0700

    Add gameOver function

--- systems/GameLoop.js
+++ systems/GameLoop.js
@@ -1,11 +1,21 @@
+import Constants from "../Constants";
+
 const randomPositions = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
+const gameOver = (head) => {
+  return (
+    head.position[0] + head.xspeed < 0 ||
+    head.position[0] + head.xspeed >= Constants.GRID_SIZE-1 ||
+    head.position[1] + head.yspeed < 0 ||
+    head.position[1] + head.yspeed >= Constants.GRID_SIZE-1)
+}
+
 export default function (entities, { events, dispatch }) {
     const head = entities.head;
     const food = entities.food;
-    
+
     if (events.length) {
       events.forEach((e) => {
         switch (e) {
@@ -36,9 +46,12 @@ export default function (entities, { events, dispatch }) {
     head.nextMove -= 1;
     if (head.nextMove === 0) {
       head.nextMove = head.updateFrequency;
-
-      head.position[0] += head.xspeed;
-      head.position[1] += head.yspeed;
+      if (gameOver(head)) {
+            dispatch("game-over");
+          } else {
+          head.position[0] += head.xspeed;
+          head.position[1] += head.yspeed;
+        }
     }
   
     return entities;
