3053bc086cb938e293fc3125efdd9a80bba49535
cb <29789422+ncbui@users.noreply.github.com>
2024-03-26 23:22:20 -0700

    Update reset button component

--- components/Reset/button.js
+++ components/Reset/button.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { View, Text } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { TapGestureHandler } from "react-native-gesture-handler";
 import { styles } from "../../systems/Styles";
 
 export default function ResetButton({ onPress }) {
   return (
     <View style={styles.resetContainer}>
         <View style={styles.resetRow}>
-            <TouchableOpacity onPress={onPress} style={styles.resetButton}>
+            <TapGestureHandler onHandlerStateChange={onPress} style={styles.resetButton}>
                 <Text style={styles.resetText}>
                     Start 
                     New 
                     Game
                 </Text>
-            </TouchableOpacity>
+            </TapGestureHandler>
         </View>
     </View>
     )}
